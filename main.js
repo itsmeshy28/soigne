07c17a5f5faadaa0f53332e914c583d07e13410a
shaela <shyshyshy29@gmail.com>
2018-01-04 11:08:51 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$('.skills-button').click(function(event) {
+		alert('An pro elit definitionem, no singulis percipitur sed, per no exerci sententiae. Mea eu modo dico, vis at veritus vocibus, cu eos quando equidem. Mel decore torquatos cu, no dico quas eum. Usu omnesque abhorreant in, pro ne illudaliquando argumentum. Labores definitiones an sea, unum soluta utroque mei id.');
+	});
+});
+
+$(document).ready(function() {
+	$('.about-button').click(function(event) {
+		alert('meowing chowing and wowing but always hungry so lay on arms while youre using the keyboard and playing with balls of wool but always hungry. Sit on the laptop scratch leg; meow for can opener to feed me for ask for petting, eat the fat cats food eat grass, throw it back up hide head under blanket so no one can see and hide when guests come over. Meow chase laser, cats secretly make all the worlds muffins so kitty loves pigs for meow to be let in. Chase laser vommit food and eat it again so why must they do that, or asdflkjaertvlkjasntvkjn (sits on keyboard');
+	});
+});
+
+$(document).ready(function() {
+	$('.follow-button').click(function(event) {
+		alert('Lorem ipsum dolor sit amet, ne viris tempor vim, in alia nostrud scripserit ius. Et mucius assentior forensibus duo, et vim nemore vituperata. No vim probo graeco virtute, ridens iisque ut vel. Sit ut nonumes fabellas.');
+	});
+});
